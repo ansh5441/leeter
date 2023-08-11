163f49dc37274b07238795c25e686e437ea11812
Anshuman <ansh5441@gmail.com>
2023-08-11 17:08:17 +0530

    Time: 77 ms (15.03%) | Memory: 7.8 MB (37.57%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.go
@@ -0,0 +1,39 @@
+
+type mapKey struct {
+	amt   int
+	index int
+}
+
+func change(amount int, coins []int) int {
+	mapOfHelp := make(map[mapKey]int)
+	// sort coins in decreasing order
+	for i := 0; i < len(coins); i++ {
+		for j := i; j < len(coins); j++ {
+			if coins[j] > coins[i] {
+				coins[i], coins[j] = coins[j], coins[i]
+			}
+		}
+	}
+
+	return changeHelp(amount, coins, 0, &mapOfHelp)
+}
+
+func changeHelp(amount int, coins []int, index int, dpMap *map[mapKey]int) int {
+	if amount < 0 {
+		return 0
+	}
+	if amount == 0 {
+		return 1
+	}
+	if val, ok := (*dpMap)[mapKey{amount, index}]; ok {
+		return val
+	}
+	numWays := 0
+	for i, c := range coins {
+		if i >= index {
+			numWays += changeHelp(amount-c, coins, i, dpMap)
+		}
+	}
+	(*dpMap)[mapKey{amount, index}] = numWays
+	return numWays
+}
