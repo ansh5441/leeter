0246cc9a06f32b49ac063ddedfcdd3ce1613c975
Anshuman <ansh5441@gmail.com>
2023-08-28 22:20:51 +0530

    Time: 59 ms (10.63%) | Memory: 16.3 MB (70.86%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.py
@@ -0,0 +1,19 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        new_arr = []
+        n = len(s)
+        j = 0
+        while j < n:
+            if j < n-1 and s[j] == 'i' and s[j+1] == "i":
+                j+=1
+            else:
+                if s[j] == "i":
+                    new_arr.reverse()
+                else:
+                    new_arr.append(s[j])
+            j += 1
+        st = ""
+        for ch in new_arr:
+            st += ch
+        return st
+            
